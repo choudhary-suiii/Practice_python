7a7d4cf52daf9f333a8e5ceae231668fe4f04e01
choudhary-suiii <choudharyshahab60@gmail.com>
2024-08-18 02:39:26 +0500

    practicing pyramid of stars
    practicing pyramid of stars

--- Python_tutorials/For_loop_learning/for_loop.py
+++ Python_tutorials/For_loop_learning/for_loop.py
@@ -1,3 +1,10 @@
 import os
-for number in range(0,100):
-    print(number,end=' ')
+space=10
+for row in range(10,0,-1):
+    space=space+1
+    print(' '*space,end='')
+    
+    for col in range(0,row):
+        
+        print("*",end='')
+    print(end='\n')
